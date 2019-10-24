469e579393769d16e0e84c3c09194b5139435854
ben <b.talbot@qut.edu.au>
2019-10-25 09:37:14 +1000

    Update old metadata

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# BenchBot
+# BenchBot API
 
-BenchBot provides a simple python-based wrapper for use with the QUT cloud-based BenchBot service
+
+The BenchBot API provides a simple interface for controlling and receiving data from a robot or simulator. [Open AI Gym](https://gym.openai.com) users will find the BenchBot API extremely familiar. By taking the hassle out of interfacing with complicated robot systems, you can focus instead on developing and testing your novel algorithms with real robot systems and realistic 3D simulators.
+
+BenchBot is used as part of the ACRV Scene Understanding Challenge. For further details of the challenge see: [https://nikosuenderhauf.github.io/roboticvisionchallenges/scene-understanding] (https://nikosuenderhauf.github.io/roboticvisionchallenges/scene-understanding)
+
+Getting started with a robot using BenchBot is as simple as:
 
 ```python
 from benchbot import BenchBot
--- setup.py
+++ setup.py
@@ -3,29 +3,24 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
-    name='benchbot',
-    version='0.1.5',
-    author='Gavin Suddrey',
-    author_email='g.suddrey@qut.edu.au',
-    description='Benchbot provides a simple python-based wrapper for use with the QUT-based BenchBot service',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=find_packages(),
-    install_requires=[
-      'numpy',
-      'opencv-python',
-      'requests'
-    ],
-    classifiers=(
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    )
-)
+setup(name='benchbot',
+      version='0.1.6',
+      author='Ben Talbot',
+      author_email='b.talbot@qut.edu.au',
+      description=
+      'The BenchBot API for use with the ACRV Scene Understanding Challenge',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=['numpy', 'opencv-python', 'requests'],
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ))
