66d96ed18632dd303fc0b2d1a584a9ac3e8417e8
ben <b.talbot@qut.edu.au>
2020-03-17 20:48:30 +1000

    Ensure valid version of scipy is installed

--- setup.py
+++ setup.py
@@ -3,27 +3,28 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='benchbot_api',
-      version='0.1.3',
-      author='Ben Talbot',
-      author_email='b.talbot@qut.edu.au',
-      description=
-      'The BenchBot API for use with the ACRV Scene Understanding Challenge',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=find_packages(),
-      install_requires=[
-          'jsonpickle', 'matplotlib', 'numpy', 'opencv-python', 'requests',
-          'scipy'
-      ],
-      classifiers=(
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "License :: OSI Approved :: BSD License",
-          "Operating System :: OS Independent",
-      ))
+setup(
+    name='benchbot_api',
+    version='0.1.3',
+    author='Ben Talbot',
+    author_email='b.talbot@qut.edu.au',
+    description=
+    'The BenchBot API for use with the ACRV Scene Understanding Challenge',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'jsonpickle', 'matplotlib', 'numpy', 'opencv-python', 'requests',
+        'scipy>=1.2.0'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ))
