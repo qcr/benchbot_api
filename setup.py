b4f470ba953917d16e1980aa16edb7f64f0e84c4
ben <b.talbot@qut.edu.au>
2020-09-20 15:50:02 +1000

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='benchbot_api',
-      version='0.1.3',
+      version='1.1.0',
       author='Ben Talbot',
       author_email='b.talbot@qut.edu.au',
       description=
