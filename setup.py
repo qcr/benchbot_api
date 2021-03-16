c4d2775d2029734571eb7a31a0fccfe817f2e8be
ben <b.talbot@qut.edu.au>
2021-03-16 16:50:10 +1000

    Update pip classifiers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='benchbot_api',
-      version='2.0.0',
+      version='2.1.0',
       author='Ben Talbot',
       author_email='b.talbot@qut.edu.au',
       description='The BenchBot API for use with the BenchBot software stack',
@@ -16,13 +16,7 @@ setup(name='benchbot_api',
           'scipy>=1.2.0'
       ],
       classifiers=(
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
           "License :: OSI Approved :: BSD License",
           "Operating System :: OS Independent",
       ))
