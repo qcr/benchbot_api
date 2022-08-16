b7339189e75eaa153934e2c0737c8a9ebae5e73c
Ben Talbot <b.talbot@qut.edu.au>
2022-08-17 05:26:17 +1000

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='benchbot_api',
-      version='2.4.1',
+      version='2.4.2',
       author='Ben Talbot',
       author_email='b.talbot@qut.edu.au',
       description='The BenchBot API for use with the BenchBot software stack',
